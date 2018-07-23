744d37fa508b23882b811e1ec58296e7922a2fe1
Lembit Lõpp <lembit.lopp@pipedrive.com>
2018-07-23 16:30:08 +0200

    Added simple server cache for 30s

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const { GraphQLServer, PubSub } = require('graphql-yoga');
 const fetch = require('node-fetch');
 
+const cacheTime = 30 * 1000;
+
+let cachedData;
+let lastFetchTime;
+
 async function fetchData() {
-  console.warn('Calling API');
-  const rawResponse = await fetch('https://3gyucw6pqd.execute-api.eu-west-1.amazonaws.com/dev');
-  return rawResponse.json();
+  if (!cachedData || Date.now() - lastFetchTime >= cacheTime) {
+    console.warn('Calling API');
+
+    const rawResponse = await fetch('https://3gyucw6pqd.execute-api.eu-west-1.amazonaws.com/dev');
+
+    cachedData = rawResponse.json();
+
+    /**
+     * Could do here:
+     * pubsub.publish('NEW_DATA', { data: cachedData });
+     * Whenever we update cache and it's different.
+     */
+
+    lastFetchTime = Date.now();
+  } else {
+    console.log('Using cache', Date.now() - lastFetchTime);
+  }
+
+  return cachedData;
 }
 
 const typeDefs = `
